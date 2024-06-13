051400b3567a8ca3ac73ce7b628f4f09a82abcee
Joaquin <joacoide.cuello@gmail.com>
2024-06-13 10:53:32 -0300

    ult act

--- modular_video_cropped.py
+++ modular_video_cropped.py
@@ -5,25 +5,27 @@ from pyzbar import pyzbar
 import time
 import numpy as np
 import csv
-
+import time
 import matplotlib.pyplot as plt
-
-
-st = time.time()  #tiempo de inicio de ejecución
-
-n_frame = 0 #numero de frame
-n_qr_total = 0  #numero total de qrs detectados en el video
-
 
 
 positions = ["left_top", "left_bottom", "right_top", "right_bottom"]
 full = []
+array_id = []
 
 
 #Abriendo CSV 
 file = open('qr_csv.csv', 'w', newline='')
 writer = csv.writer(file)
 writer.writerow(['frame', 'qr', 'esquina', 'x', 'y'])   #primera fila del CSV 
+
+dt_qr = []      #lista de cantidad de QRs reconocidos por iteracion
+dt_x = []       #valores en x para crear heatmap
+dt_y = []       #valores en y para crear heatmap
+dv = []         #Este arreglo recibe los divisores de altura arbitrarios. Ahora va de 1,6
+
+sub_list = []       #sub_list es la lista que contiene, para un valor de x, todas las combinaciones con valores de 
+elapsed_time = 0
 
 # ------ FUNCIONES ------
 
@@ -40,7 +42,6 @@ def convert_lists(pts):      #recibe coordenadas de las esquinas del poligono
     return arr_np, arr                              # retorna dupla (lista de numpy, lista comun)
 
 def draw_polygons(imagen, coordenate_np, coordenate_list):   #Dibujar poligonos en los frames de un nuevo video
-
     #Clase que se llama point. clase hija de point 2d y 3d
     cv2.polylines(imagen, [coordenate_np], True, (0,0,255), 2)  #dibujar poligono que encierra qr - esto es solo para visualizar en el video
 
@@ -49,8 +50,8 @@ def draw_polygons(imagen, coordenate_np, coordenate_list):   #Dibujar poligonos 
         cv2.circle(imagen, (coordenate_list[i][0],coordenate_list[i][1]), radius=5, color=(0, 255, 255), thickness=-1) #left_top, left_down, right_top, right_down
 
     #Escribir imagen en el video
-    out.write(image)
-    cv2.imshow('video', image)   #muestra un frame del video en pantalla
+    out.write(imagen)
+    #cv2.imshow('video', imagen)   #muestra un frame del video en pantalla
 
 def csv_write(frame, data, positions, coordenate_list, x , y):     #Escribir en CSV
     for i in range(0,4):
@@ -64,154 +65,168 @@ def sort_position(coordenate_list):     #Corregir posiciones de los poligonos
 
     return sorted_list
 
-def generate_heatmap(data_qr, data_x, data_y):     #data_qr es la cantidad de qr reconocidos por iteracion (con un cropped distinto)
+def generate_heatmap(data, data_qr, data_x, data_y, strategy):     #data_qr es la cantidad de qr reconocidos por iteracion (con un cropped distinto)
+    if(strategy == 0):
+        _y= data_y[::-1]                               #Se da vuelta la lista de y para que se imprima de forma ascendente. Antes era data__y.reverse()
+        print(len(_y))
 
-    _y= data_y[::-1]                               #Se da vuelta la lista de y para que se imprima de forma ascendente. Antes era data__y.reverse()
-    print(len(_y))
+        fig, ax = plt.subplots(figsize = (16,12))
+        
+        ax.set_xticks(np.arange(len(data_x)), labels=data_x)
+        ax.set_yticks(np.arange(len(_y)), labels=_y)
 
-    fig, ax = plt.subplots(figsize = (16,12))
-    
-    ax.set_xticks(np.arange(len(data_x)), labels=data_x)
-    ax.set_yticks(np.arange(len(_y)), labels=_y)
-
-    for i in range(0, len(data_qr)):        #Logica para normalizar los valores de la lista (para que sean entre 0 y 1)
-        for j in range(0,len(data_qr[i])):
-            data_qr[i][j] = data_qr[i][j]/100
-
-
-    #Ordenar datos para graficar
-    data_qr = np.transpose(np.array(data_qr))  #Cambia columna por fila
-    data_qr = data_qr.tolist()     
-    data_qr.reverse()                          #Este invierte los elementos de la lista
-    print(data_qr)
-
-    ax.imshow(data_qr, cmap = "Blues") 
-
-    plt.xlabel('ancho de recorte')
-    plt.ylabel('alto de recorte')
-    plt.savefig('heatmap.png')
-    plt.show()
+        for i in range(0, len(data_qr)):        #Logica para normalizar los valores de la lista (para que sean entre 0 y 1)
+            for j in range(0,len(data_qr[i])):
+                data_qr[i][j] = data_qr[i][j]/100
 
 
-def cropp_frame(imagen, div_h, div_w, h, w):
+        #Ordenar datos para graficar
+        data_qr = np.transpose(np.array(data_qr))  #Cambia columna por fila
+        data_qr = data_qr.tolist()     
+        data_qr.reverse()                          #Este invierte los elementos de la lista
+        print(data_qr)
+
+        ax.imshow(data_qr, cmap = "Blues") 
+
+        plt.xlabel('ancho de recorte')
+        plt.ylabel('alto de recorte')
+        plt.savefig('heatmap.png')
+        plt.show()
+    if(strategy == 1):
+        # Guardar valores de coorednadas y cantidad de QR en arreglos independientes
+        y_coords = [d[0] for d in data]
+        x_coords = [d[1] for d in data]
+        values = [d[2] for d in data]
+
+        heatmap_size = (max(y_coords) + 1, max(x_coords) + 1)   # Calcular el tamaño del heatmap
+        heatmap = np.zeros(heatmap_size)    # Crear una matriz para almacenar los valores del heatmap
+
+        # Reemplazar los valores de 0 por Cantidad de QR
+        for y, x, value in data:
+            heatmap[y, x] = value
+
+        # Crear el heatmap
+        plt.figure(figsize=(8, 6))
+        plt.imshow(heatmap,cmap = "Blues")
+
+        # Añadir las etiquetas de los ejes
+        plt.xticks(np.arange(heatmap_size[1]), np.arange(heatmap_size[1]))
+        plt.yticks(np.arange(heatmap_size[0]), np.arange(heatmap_size[0]))
+
+        # Añadir los valores dentro de los recuadros del heatmap
+        for y in range(heatmap_size[0]):
+            for x in range(heatmap_size[1]):
+                if heatmap[y, x] != 0:
+                    plt.text(x, y, int(heatmap[y, x]), ha='center', va='center', color='white')
+
+        # Mostrar el heatmap
+        plt.title('Heatmap')
+        plt.xlabel('Divisor de desplazamiento en X')
+        plt.ylabel('Divisor de desplazamiento en Y')
+        plt.savefig('divisores_heatmap.png')
+        plt.show()
+
+def cropp_frame(imagen, div_h, div_w, h, w, desp_x, desp_y):
     x = 0
     y = 0
-    global n_qr_total
 
-    
-    for i in range(0,div_w):                            #Se itera tantas veces se dividio el ancho  
-        for j in range(0,div_h):                        #Se itera tantas veces se dividio el largo
-        
+    qr_frame = []
+
+    global n_qrtotal
+    for i in range(0,div_w*div_desp_x):                            #Se itera tantas veces se dividio el ancho  
+        for j in range(0,div_h*div_desp_y):                        #Se itera tantas veces se dividio el largo
             crop_img = imagen[y:y+h, x:x+w]          #crop image es la imagen recortada (en forma de arreglo de numpy)
-            #cv2.imshow("cropped", crop_img)         #se muestra en pantalla la imagen recortada
 
             barcodes = pyzbar.decode(crop_img)      #Reconoce todos los QRs de una imagen. Devuelve una lista
-
+            
             for k, barcode in enumerate(barcodes, start=1):
-        
+                
                 # Decodificamos el tipo del código, así como la información que contiene.
                 data = barcode.data.decode('utf-8')
-                type_ = barcode.type
                 coordenate_polygon = barcode.polygon 
                 
-                # Imprimimos los datos del código de barras/QR
-                #print(f'Información del código de barra #{k}: {data}')
-                #print(f'Tipo del código de barra #{k}: {type_}')
-                n_qr_total = n_qr_total + 1
-                #print("Cantidad de QRs escaneados: ", n_qr_total)
+                if not data in qr_frame: 
+                    qr_frame.append(data)
+                    #Logica para pasar los puntos devueltos por el poligono a una lista comun de python
+                    arr_np, arr = convert_lists(coordenate_polygon)     #Ver funcion
 
-                #Logica para pasar los puntos devueltos por el poligono a una lista comun de python
-                arr_np, arr = convert_lists(coordenate_polygon)     #Ver funcion
+                    corrected_arr = sort_position(arr)
 
-                corrected_arr = sort_position(arr)
-
-
-                draw_polygons(crop_img, arr_np, corrected_arr)                #Ver funcion
-                
-                csv_write(n_frame, data, positions, corrected_arr, x, y)            #Ver funcion
-                #cv2.waitKey(1)
-                
-            y = y + h   #Se incrementa la posicion base del cropped en altura
-
+                    n_qrtotal = n_qrtotal + 1
+                    #draw_polygons(crop_img, arr_np, corrected_arr)                #Ver funcion
+                            
+                    #cv2.imshow("cropped", crop_img)
+                    csv_write(n_frame, data, positions, corrected_arr, x, y)            #Ver funcion
+                    cv2.waitKey(1)
+        
+            y = y + desp_y  #Se incrementa la posicion base del cropped en altura
         y = 0        #HIJO DE PUTA - Se tiene que resetear a cero la posicion en altura
-        x = x + w   #Se incrementa la posicion base del cropped en ancho
+        x = x + desp_x   #Se incrementa la posicion base del cropped en ancho
+
 # ------------------------
 
-dt_qr = []      #lista de cantidad de QRs reconocidos por iteracion
-dt_x = []       #valores en x para crear heatmap
-dt_y = []       #valores en y para crear heatmap
-dv = []         #Este arreglo recibe los divisores de altura arbitrarios. Ahora va de 1,6
-sub_list = []
-
-for i in range(4,5):
-    dv.append(i)
-
-    #[1,2,3,4,5,6]
-    
-print(dv)
-print(len(dv))
-
-
-sub_list = []       #sub_list es la lista que contiene, para un valor de x, todas las combinaciones con valores de y
-
-
-
-elapsed_time = 0
-captura = cv2.VideoCapture("VID_20230322_173621.mp4")
-
-# Seteamos la resolucion casteando de float a int. 
-frame_width = int(captura.get(3)) 
-frame_height = int(captura.get(4)) 
-size = (frame_width, frame_height)
-
-# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 29.458, size)
-
 #CUIDADO CON ESTO !!!!!!!
-div_h = 5        # divisor de altura
-div_w = 3           # divisor de ancho
+desp_list = []
 
-h = int(frame_height/div_h)   #divide en altura y ancho segun la resolucion del video
-w = int(frame_width/div_w)  
-
-valores = [div_h, div_w, h, w]
-print(valores)
-
-#Datos divisores
-dt_x.append(str(div_w))     #Se agregan como string para usarlos como labels en el heatmap
-dt_y.append(str(div_h))
-
-n_qr_total = 0
-
-while (captura.isOpened()):
-
-    ret, image = captura.read() #image es un frame del video
-
-    if ret == True:
+all_qrs = []
+for i in range(2,8):
         
-        cropp_frame(image, div_h, div_w, h , w)
-        n_frame += 1    #se incrementa el numero de frame
         
-        cv2.waitKey(1)
-    else: break
+        st = time.time()  #tiempo de inicio de ejecución
 
-et = time.time()  #tiempo de finalizado de ejecución
+        n_frame = 0 #numero de frame
+        captura = cv2.VideoCapture("VID_20230322_173621.mp4")
 
-elapsed_time = et - st      #tiempo total que tarda el programa
-print('Tiempo ejecutado cropped:', elapsed_time, 'seconds')
+        # Seteamos la resolucion casteando de float a int. 
+        frame_width = int(captura.get(3)) 
+        frame_height = int(captura.get(4)) 
+        size = (frame_width, frame_height)
 
-sub_list.append(n_qr_total)  
+        # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+        out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 29.458, size)
+        
+        n_qrtotal = 0
 
+        div_h = 5          # divisor de altura
+        div_w = 3          # divisor de ancho
+
+        div_desp_y = i
+        div_desp_x = i+1
+
+        h = int(frame_height/div_h)   #divide en altura y ancho segun la resolucion del video
+        w = int(frame_width/div_w)  
+
+        desp_y = int(h/div_desp_y) 
+        desp_x = int(w/div_desp_x) 
+
+        #Datos divisores
+        dt_x.append(str(div_w))     #Se agregan como string para usarlos como labels en el heatmap
+        dt_y.append(str(div_h))
+
+        print("---------------------Iniciando proceso con: ", div_desp_x, ' y ', div_desp_y)
+        while (captura.isOpened()):
+            ret, image = captura.read() #image es un frame del video
+            if ret == True:
+                
+                cropp_frame(image, div_h, div_w, h , w, desp_x, desp_y)
+                n_frame += 1    #se incrementa el numero de frame
+                
+                #if(n_frame % 10 == 0):
+                #    print('frame ',n_frame)
+                
+
+                cv2.waitKey(1)
+            else:
+                break
+
+        desp_list.append([div_desp_y,div_desp_x,n_qrtotal]) 
+        print(desp_list)
+        et = time.time()  #tiempo de finalizado de ejecución
+
+        elapsed_time = et - st      #tiempo total que tarda el programa
+        
+        print('Tiempo ejecutado para divisor en x ', div_desp_x, ' divisor en y ',div_desp_y,': ', elapsed_time, 'second, qr totales: ', n_qrtotal)
+
+generate_heatmap(desp_list,[],[],[],1)
 captura.release()
 cv2.destroyAllWindows()
-
-
-dt_qr.append(sub_list)    #dt_qr es el arreglo con la cantidad de QRs reconocidos para combinaciones de x e y
-
-
-# Heat map
-print(dt_qr)
-print(dt_x)
-print(dt_y)
-if(len(dv)> 4):                     #si el tamaño de la lista es menor a 4 entonces no genera heatmap debido a que necesitas mas de 2 colores para pintar
-    generate_heatmap(dt_qr, dv, dv)        
--- most_watched.py
+++ most_watched.py
@@ -1,4 +1,5 @@
 import csv
+import math
 
 infoqr = []
 dictqr = {}
@@ -22,16 +23,47 @@ def mostWatched(name): #Retorna un diccionario: key -> id_qr; value -> int: apar
                 dictqr[id] = 0
             else:
                 dictqr[id] = dictqr[id] + 1
-    return dict(sorted(dictqr.items(), key=lambda x:x[1], reverse=True))
+    return dict(sorted(dictqr.items(), key=lambda x:x[1], reverse=True))    #Entender como ordena
 
+def mostWatchedbyColumn(dictQR):        
+    columnDict = createColumnDict(dictQR)
+    columnDict = {}
+    for id in dictQR:
+        columnNumber = clm(id)
+        if columnNumber in columnDict:
+            if dictQR[columnDict[columnNumber]] < dictQR[id]:
+                columnDict[columnNumber] = id
+            #print("el id "+id +" tiene menos visiones que "+columnDict[columnNumber])
+        else:
+            columnDict[columnNumber] = id
+
+    sorted_col = dict(sorted(columnDict.items()))
+    print(sorted_col)
+    #print(sorted_col)
+    #sorted_dictQR = {k: dictQR[k] for k in sorted(dictQR, key=lambda x: int(x))} #Entender como ordena
+    #print(sorted_dictQR)
+    return sorted_col
+
+def clm(qrId):
+    return math.trunc((int(qrId) - 71)/7)
+
+def createColumnDict(DictQr):
+    keysId = list(DictQr.keys())
+    columnDict = {}
+    for qrId in keysId:
+        columnNumber = clm(qrId)
+        if columnNumber in columnDict:
+            columnDict[columnNumber] = columnDict[columnNumber] + 1
+        else:
+            columnDict[columnNumber] = 0
+    return dict(sorted(columnDict.items()))
 
 def createCSVFile(rank, dictQR, strategy): #Crea archivo CSV dependiendo de la estrategia seleccionada
-
     for i in range(0,rank):
         keys.append(list(dictQR.keys())[i])
     if (strategy == 0):
-        fileName = 'qr_'+ id +'.csv'
         for id in keys: # '80'
+            fileName = 'qr_'+ id +'.csv'
             with open(fileName, 'w', newline='') as file: # qr_80.csv
                 writer = csv.writer(file) 
                 writer.writerow(['frame', 'qr','esquina','x','y']) # escribe header
@@ -42,6 +74,7 @@ def createCSVFile(rank, dictQR, strategy): #Crea archivo CSV dependiendo de la e
                         if(row[1] == id):
                             writer.writerow(row)
             print("Se creo archivo " + fileName)
+
     if (strategy == 1):
         fileName = 'ranking_top_'+ str(rank) +'_qr_.csv'
         infoqr = []
@@ -61,12 +94,35 @@ def createCSVFile(rank, dictQR, strategy): #Crea archivo CSV dependiendo de la e
             writer.writerow(['frame', 'qr','esquina','x','y']) # escribe header
             for row in listSorted:
                 writer.writerow(row)
-        print("Se creo archivo " + fileName)    
+        print("Se creo archivo " + fileName)
+        
+    if (strategy == 2):
+        idColumn = list(mostWatchedbyColumn(dictQR).values())
+        fileName = 'ranking_by_col.csv'
+        infoqr = []
+
+        print(idColumn)
+        for id in idColumn:
+            print("Iniciando proceso qr_id: " + id)
+            with open('qr_csv.csv') as file_obj: #Abre archivo
+                reader_obj = csv.reader(file_obj) 
+                for row in reader_obj:
+                    if(row[1] == id):
+                        infoqr.append(row)
+        listSorted = sorted(infoqr, key=lambda x: int(x[0]))
+        with open(fileName, 'w', newline='') as file:
+            writer = csv.writer(file) 
+            writer.writerow(['frame', 'qr','esquina','x','y']) # escribe header
+            for row in listSorted:
+                writer.writerow(row)
+        print("Se creo archivo " + fileName)
 
 def main():
     dictqr = mostWatched('qr_csv.csv')
+    print(dictqr)
     # primer parametro(rank):       cantidad de qr mas vistos a guardar en CSV
     # tercer parametro(strategy):   0 -> multi archivos , 1 -> unico archivo
-    createCSVFile(3,dictqr,1)
-
+    #createCSVFile(3,dictqr,0)
+    #createCSVFile(3,dictqr,1)
+    #createCSVFile(3,dictqr,2)
 main()
